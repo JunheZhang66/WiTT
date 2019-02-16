9312b71bade7d4aa267b9a8be5a8224101a91adc
mengtliu <mengting.liu@stonybrook.edu>
2019-02-15 22:59:00 -0500

    Translates text base on the given two languages, one to be translated from and one to be translated to, using Google Cloud API.

--- app/src/main/java/witt/CloudTranslateAPI.java
+++ app/src/main/java/witt/CloudTranslateAPI.java
@@ -1,12 +1,16 @@
 package witt;
+import com.google.cloud.translate.Language;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.Translate.TranslateOption;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
-import com.google.cloud.translate.Detection;
-import java.util.LinkedList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CloudTranslateAPI {
-    private static final String [] LANGUAGES ={
+    /*private static final String [] LANGUAGES ={
             "af", "sq", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb",
             "ny", "zh-TW", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr",
             "gl", "ka", "de", "el", "gu", "ht", "ha", "iw","hi", "hmn", "hu", "is",
@@ -14,26 +18,18 @@ public class CloudTranslateAPI {
             "la", "lv", "lt", "mk", "mg", "ms", "ml","mt", "mi", "mr", "mn", "my", "ne", "no",
             "fa", "pl", "pt", "ro", "ru", "sr", "st", "si", "sk", "sl", "so", "es", "su",
             "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "yi", "yo", "zu"
-    };
+    };*/
 
-    //googleapis git hub
     Translate trans = TranslateOptions.getDefaultInstance().getService();
     List <Language> languages = trans.listSupportedLanguages();
-    Set<String> supportedLanguages = new HashSet<>();
-    for(Language language : languages){
-        supportedLanguages.add(language.getCode());
-        assertNotNull(Language.getName());
+    Map<String,String> all = new HashMap<>();
+    public CloudTranslateAPI(){
+        for(Language language : languages){
+            all.put(language.getName(),language.getCode());
+        }
     }
-    for(String code : LANGUAGES){
-        assertTrue(supportedLanguages.contains(code));
-    }
-
-
-    public static String translate(String lang1, String lang2, String text){
-        List<String> texts = new LinkedList<>();
-        texts.add(lang1);
-        texts.add(lang2)
-        Translation translation = trans.translate(text, TranslateOption.sourceLanguage(texts.get(0).getCode()),TranslateOption.targetLanguage(texts.get(1).getCode()));
+    public String translate(String lang1, String lang2, String text){
+        Translation translation = trans.translate(text, TranslateOption.sourceLanguage(all.get(lang1)),TranslateOption.targetLanguage(all.get(lang2)));
         String fin = translation.getTranslatedText();
         return fin;
     }
